a6f520ec873a2b1fcef5f71f7ed3d1386dd8f2fc
wissem <wissemhachem042@gmail.com>
2025-12-04 23:17:23 +0100

    inital

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        test changement
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
